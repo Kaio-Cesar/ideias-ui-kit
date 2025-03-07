570f504f230bb0bd9fe905cb8cb6a48384bc8a91
Kaio-Cesar <git@kcesar.dev>
2025-03-06 23:49:02 -0300

    Forgoted about tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 import dts from 'vite-plugin-dts'
 import { resolve } from 'node:path'
 
@@ -14,7 +14,7 @@ let currentFormat = ''
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    tailwindcss(),
     dts({
       tsconfigPath: 'tsconfig.build.json',
       cleanVueFileName: true,

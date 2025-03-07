2ba4e1dfea77fb82326cac8468f17cd944dc91a7
Kaio-Cesar <git@kcesar.dev>
2025-03-06 23:12:16 -0300

    Hopefully, fixed the lib

---
+++ src/layout/BasePanel.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-bind="$attrs" :class="cn(panel({ style }), className)">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { tv } from 'tailwind-variants'
+
+import { cn } from '@/lib/utils'
+
+const panel = tv({
+  base: 'rounded-lg border-2 border-zinc-300 dark:border-zinc-600',
+  variants: {
+    style: {
+      primary: 'bg-zinc-100 dark:bg-zinc-900',
+      glass: 'bg-neutral-200/30 backdrop-blur-md',
+    },
+  },
+  defaultVariants: {
+    style: 'primary',
+  },
+})
+
+const { class: className, style } = defineProps<{
+  class?: HTMLAttributes['class']
+  style?: 'glass'
+}>()
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,79 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'node:path'
+
+import pkg from './package.json'
+
+let currentFormat = ''
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    dts({
+      tsconfigPath: 'tsconfig.json',
+      cleanVueFileName: true,
+      exclude: ['src/__test__/**', 'src/**/__story__/**', 'src/**/*.story.vue'],
+      rollupTypes: true,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    dedupe: ['vue', '@vue/runtime-core'],
+  },
+  build: {
+    minify: false,
+    target: 'esnext',
+    sourcemap: true,
+    lib: {
+      name: 'ideias-ui-kit',
+      formats: ['cjs', 'es'],
+      fileName: (format, name) => {
+        currentFormat = format
+        return `${name}.${format === 'es' ? 'js' : 'cjs'}`
+      },
+      entry: {
+        index: resolve(__dirname, 'src/index.ts'),
+      },
+    },
+    rollupOptions: {
+      external: [...Object.keys(pkg.dependencies ?? {})],
+      output: {
+        format: 'esm',
+        inlineDynamicImports: false,
+        // preserveModules: true,
+        // preserveModulesRoot: 'src',
+        manualChunks: (moduleId, meta) => {
+          const info = meta.getModuleInfo(moduleId)
+          if (!info?.isIncluded) {
+            return null
+          }
+
+          const parts = moduleId.split('?')[0].split('/')
+          if (parts.length < 2) {
+            return null
+          }
+
+          const [namespace, file] = parts.slice(-2)
+          const result = `${namespace}/${file.slice(0, file.lastIndexOf('.'))}`
+          return result
+        },
+
+        exports: 'named',
+        chunkFileNames: (chunk) => `${chunk.name}.${currentFormat === 'es' ? 'js' : 'cjs'}`,
+        assetFileNames: (chunkInfo) => {
+          console.log(chunkInfo.names)
+          if (chunkInfo.names[0] === 'style.css') {
+            return 'index.css'
+          }
+          return chunkInfo.names[0] as string
+        },
+      },
     },
   },
 })
